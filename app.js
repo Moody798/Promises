58aeca76e75f25264981d95af542893501aa3c93
Mohamed Ahmed Fouad <moodymohamed798@gmail.com>
2022-12-31 19:05:55 +0200

    then

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hi");
+let emailRef = document.querySelector(".email");
+console.log(emailRef);
+fetch("https://jsonplaceholder.typicode.com/users/1").then(response =>{
+    response.json().then(data =>{
+        console.log(data);
+        emailRef.innerHTML = data.email
+    });
+})
