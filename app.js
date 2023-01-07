ea0b453bcd2cc8b826e67afab0ed4e1b38866e94
Mohamed Ahmed Fouad <moodymohamed798@gmail.com>
2023-01-07 16:01:05 +0200

    creating promises

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-let emailRef = document.querySelector(".email");
-console.log(emailRef);
-fetch("https://jsonplaceholder.typicode.com/users/1").then(response =>{
-    response.json().then(data =>{
-        console.log(data);
-        emailRef.innerHTML = data.email
-    });
-})
+let statusRef = document.querySelector(".sub");
+let vedioRef = document.querySelector(".video");
+function sub() {
+  return new Promise((resolve, reject) => {
+    setTimeout(()=>{resolve(undefined);},2000)
+    
+  });
+}
+function getVideo(subscriptionStatus) {
+  return new Promise((resolve, reject) => {
+    if (subscriptionStatus === "VIP") {
+      resolve("Show video");
+    } else if (subscriptionStatus === "FREE") {
+      resolve("show trailer");
+    } else {
+      reject("no video");
+    }
+  });
+}
+async function main() {
+  let status = await sub();
+  statusRef.innerHTML = status;
+  try {
+    await getVideo(status);
+  } catch (e) {
+    vedioRef.innerHTML= (e);
+  }
+  
+}
+main();
